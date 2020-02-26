4da20ce851600a66e12ca9e0356cacef2fd979ce
Raspberry_Pi Pakistan <36759636+SyedUmaidAhmed@users.noreply.github.com>
2020-02-26 23:52:50 +0500

    Add files via upload

---
+++ ser_eff_2.py
@@ -0,0 +1,20 @@
+import pigpio
+import time
+
+pi = pigpio.pi()
+pi.set_mode(18, pigpio.OUTPUT)
+
+print ("mode: ", pi.get_mode(18))
+
+for i in range(0,5):
+    print("setting to: ",pi.set_servo_pulsewidth(18, 1000))
+    print("set to: ",pi.get_servo_pulsewidth(18))
+
+    time.sleep(1)
+
+    print("setting to: ",pi.set_servo_pulsewidth(18, 2000))
+    print("set to: ",pi.get_servo_pulsewidth(18))
+
+    time.sleep(1)
+
+pi.stop()
